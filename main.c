81444398ae7ef7ed09d214618911ca1188144bbe
Erwan Pillard <erwan.pillard@edu.ece.fr>
2021-12-16 16:27:41 +0100

    partie graphique

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+
+#include <allegro5/allegro.h>
+
 #include "menu.h"
 
-int main(){
-    menuPrincipale();
-    return 0;
-}
 
+
+int main(){
+    ALLEGRO_DISPLAY* display = NULL;
+
+    assert(al_init());
+
+    display = al_create_display(400, 600);
+    assert(display != NULL);
+
+    al_set_window_title(display, "Monopoly");
+    al_flip_display();
+}
