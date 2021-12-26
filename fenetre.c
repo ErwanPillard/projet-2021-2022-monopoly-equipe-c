b869b7396fd19032dbf39c0ac5a08793b87759a3
Erwan Pillard <erwan.pillard@edu.ece.fr>
2021-12-26 18:19:05 +0100

    menu

--- fenetre.c
+++ fenetre.c
@@ -2,38 +2,39 @@
 #include <allegro5/allegro_image.h>
 #include <assert.h>
 #include "fenetre.h"
-#include "menu.h"
 #include <stdio.h>
 
+
 int setWindow(){
-    ALLEGRO_DISPLAY* display = NULL;
-    ALLEGRO_BITMAP *plateau = NULL;
+    ALLEGRO_DISPLAY* display;
+
+    if(!al_init()){
+        printf("Erreur initialisation");
+    }
 
 
-    //variables
-    int width = 640;
-    int height = 480;
-    //int isEnd = 0;
+    display = al_create_display(WIDTH, HEIGHT);
 
-    assert(al_init());
-    assert(al_init_image_addon());
+    if(!display){
+        printf("Erreur création display");
+    }
 
-    display = al_create_display(width, height);
     al_set_window_position(display,10,10);
     al_set_window_title(display, "Monopoly");
 
-    plateau = al_load_bitmap("../images/case1.jpg");
+    if(!al_init_image_addon()){
+        printf("Erreur d'initialisation addon");
+    }
+    ALLEGRO_BITMAP *plateau = NULL;
+
+    plateau = al_load_bitmap("../images/Capture3.PNG");
 
     if(plateau == NULL) printf("Le chemin est pas bon");
 
-    /*
-    while(!isEnd){
-        if(quitter() == true){
-            isEnd = 1;
-        }
-    }
+    al_flip_display();
+    al_rest(5);
     al_destroy_bitmap(plateau);
-    al_destroy_display(display);*/
+    al_destroy_display(display);
 
 
 }
